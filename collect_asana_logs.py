60e1c4f5566abd4b344503ad9e53f375013e7bee
Pushkarev Dmitrii <63014898+qwerty1q2w@users.noreply.github.com>
2024-01-25 14:17:49 +0200

    Update collect_asana_logs.py

--- collect_asana_logs.py
+++ collect_asana_logs.py
@@ -1,43 +1,54 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import json
 import requests
-import sys
-import os
 import configparser
+from urllib.parse import quote
+import datetime
 
+# Get the current date and time in a specific format
+dt = datetime.datetime.utcnow()
+formatted_date_with_microseconds = dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+now_date = formatted_date_with_microseconds[:-4] + 'Z'
+# format 2024-01-25T11:47:11.895Z
+
+# Read configuration file
 config = configparser.ConfigParser()
 config.read('config')
 
+# Retrieve necessary configuration parameters
 org_id = config['asana']['org_id']
 token = config['asana']['token']
 log_path = config['asana']['log_path']
 counter_file_path = config['asana']['counter_file_path']
 
+# Read the last processed time from the counter file
+# format 2024-01-25T11:47:11.895Z
+with open(counter_file_path, "r") as f:
+    last_time = f.read().strip()
 
-counter_file = open(counter_file_path)
-cursor = counter_file.read().replace("\n","")
-counter_file.close()
-
-url = 'https://app.asana.com/api/1.0/workspaces/%s/audit_log_events?offset=%s' % (org_id,cursor)
-#url = 'https://app.asana.com/api/1.0/workspaces/%s/audit_log_events' % (org_id) ### use when offset is expired. Once
-
-headers = {'Authorization': 'Bearer %s' % token, 'Accept': 'application/json'}
-
+# Initial API request to fetch audit log events
+url = f'https://app.asana.com/api/1.0/workspaces/{org_id}/audit_log_events?start_at={quote(last_time)}&end_at={quote(now_date)}'
+headers = {'Authorization': f'Bearer {token}', 'Accept': 'application/json'}
 response = requests.get(url, headers=headers)
-#print(response.text)
-#print(response.status_code)
-if response.status_code == 200:
-    ss = response.json()
-    if len(ss['data']) > 0:
-        for i in ss['data']:
-            with open(log_path, 'a') as f:
-                f.write(json.dumps(i) + '\n')
-        f.close()
+data = response.json()
+logs = data.get('data', [])
+page_token = data.get('next_page', {}).get('offset', None)
 
-else:
-#    print(response.text)
-    sys.exit()
+# Loop to handle pagination
+while page_token:
+    url = f'https://app.asana.com/api/1.0/workspaces/{org_id}/audit_log_events?start_at={quote(last_time)}&end_at={quote(now_date)}&offset={page_token}'
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    logs.extend(data.get('data', []))
+    page_token = data.get('next_page', {}).get('offset', None)
+    if len(data['data']) == 0:
+        page_token = None
 
-with open(counter_file_path, 'w') as counter_file:
-        counter_file.write(ss['next_page']['offset'])
-counter_file.close()
+# Writing logs to the file
+with open(log_path, 'a') as log_file:
+    for log_entry in logs:
+        log_file.write(json.dumps(log_entry) + '\n')
+
+# Update the counter file with the current date
+with open(counter_file_path, "w") as f:
+    f.write(now_date)
